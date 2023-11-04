bcecde2880165e1744774dac3838abb34156fb8a
evaneryanto <evaneryanto95@gmail.com>
2023-11-05 00:05:06 +0700

    edit migration pesanan dan migration user

--- database/migrations/2023_11_04_164733_create_pesanans_table.php
+++ database/migrations/2023_11_04_164733_create_pesanans_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('pesanans', function (Blueprint $table) {
             $table->id();
+            $table->integer('id_user');
             $table->timestamps();
         });
     }
