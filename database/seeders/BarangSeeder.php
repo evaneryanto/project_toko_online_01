537344a790fc96eaf0a5c50a815cd9e146f6795b
evaneryanto <evaneryanto95@gmail.com>
2023-11-06 00:00:57 +0700

    edit all seeder

--- database/seeders/BarangSeeder.php
+++ database/seeders/BarangSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\User;
+use App\Models\Barang;
 
 class BarangSeeder extends Seeder
 {
@@ -13,13 +13,39 @@ class BarangSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([
-            'nama_barang' => 'Baju bola putih moti kotak',
-            'stok_barang' => 10,
-            'harga_barang' => 80000,
-            'keterangan'=>'Baju bola warna putih dengan motf batik kotak warna hitam ukuran XL ',
-            'created_at' => date('Y-m-d H:i:s',time()),
-            'updated_at' => date('Y-m-d H:i:s',time())
-        ]);
+        $arr = [
+            [
+                'nama_barang' => 'Baju bola putih motif kotak',
+                'stok_barang' => 10,
+                'harga_barang' => 80000,
+                'gambar' => 'baju_01.jpeg',
+                'keterangan'=>'Baju bola warna putih dengan motf batik kotak warna hitam ukuran XL ',
+                'created_at' => date('Y-m-d H:i:s',time()),
+                'updated_at' => date('Y-m-d H:i:s',time())
+            ],
+            [
+                'nama_barang' => 'Baju bola putih motif spiral',
+                'stok_barang' => 15,
+                'harga_barang' => 70000,
+                'gambar' => 'baju_02.jpeg',
+                'keterangan'=>'Baju bola warna putih dengan motf spiral warna merah ukuran XXL ',
+                'created_at' => date('Y-m-d H:i:s',time()),
+                'updated_at' => date('Y-m-d H:i:s',time())
+            ],
+            [
+                'nama_barang' => 'Baju bola hitam motif spiral ungu',
+                'stok_barang' => 20,
+                'harga_barang' => 90000,
+                'gambar' => 'baju_03.jpeg',
+                'keterangan'=>'Baju bola warna hitam dengan motif spiral ungu ukuran XXL ',
+                'created_at' => date('Y-m-d H:i:s',time()),
+                'updated_at' => date('Y-m-d H:i:s',time())
+            ]
+            
+        ];
+        foreach($arr as $key=> $row){
+           Barang::create($row);
+       }
+      
     }
 }
